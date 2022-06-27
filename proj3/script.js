399e5b1c833d7cb7352d63acf1a8b6ecc98ca0b0
Jaya <akulajayateja4444@gmail.com>
2022-06-27 19:17:09 +0530

    Added few more courses and we can sort according to the price

--- proj3/script.js
+++ proj3/script.js
@@ -18,4 +18,32 @@ const courses = [
         name: "Complete MERN stack course",
         price: 899,
     }
-]
+];
+
+function generateList() {
+    const ul  = document.querySelector(".list-group")
+    ul.innerHTML = "";
+    courses.forEach(course => {
+        const li = document.createElement("li");
+        li.classList.add("list-group-item");
+
+        const name = document.createTextNode(course.name);
+        li.appendChild(name);
+
+        const span = document.createElement("span");
+        span.classList.add("float-right");
+        const price = document.createTextNode("Rs " + course.price);
+        span.appendChild(price);
+
+        li.appendChild(span);
+        ul.appendChild(li);
+    });
+}
+
+// generateList();
+window.addEventListener("load", generateList);
+const button = document.querySelector(".sort-btn");
+button.addEventListener("click", () => {
+    courses.sort((a, b) => a.price - b.price);
+    generateList();
+});
