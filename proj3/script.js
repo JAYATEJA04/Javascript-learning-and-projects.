300a5c7a5ec8782dbccf40394caf6543c81b4bd6
Jaya <akulajayateja4444@gmail.com>
2022-06-27 16:15:50 +0530

    another commit in standby

---
+++ proj3/script.js
@@ -0,0 +1,21 @@
+const courses = [
+    {
+        name: "Complete ReactJS course",
+        price: 299,
+    },
+
+    {
+        name: "Complete HTML and CSS course",
+        price: 399,
+    },
+
+    {
+        name: "Complete JavaScript course",
+        price: 199,
+    },
+
+    {
+        name: "Complete MERN stack course",
+        price: 899,
+    }
+]
